7bc3e528ded4f02565c6bb380fcc9c5c8edc1aaf
dniel <daniel@engfeldt.net>
2021-03-30 09:17:20 +0200

    add Set Version bash script

---
+++ docker/scripts/version.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+################################################################################
+# Help
+################################################################################
+Help() {
+  # Display Help
+  echo "Update Version so that Terraform knows which version to deploy."
+  echo ""
+  echo "Usage:"
+  echo "  <version>                 - The new version"
+  echo "  <application>             - The application to set version for"
+  echo "  <name prefix>             - The name prefix to set version for"
+  echo "  -h                        - Display this message."
+  echo ""
+  echo "Syntax: ./version.sh <version> <application> <name prefix>"
+  echo
+}
+
+################################################################################
+# Set Version                                                                        #
+################################################################################
+SetVersion() {
+  local version=$1
+  local application=$2
+  local nameprefix=$1
+  echo "Set version '$version' for '$application' in '$nameprefix'"
+  aws ssm put-parameter --name "/versions/$nameprefix/$application" --type "String" --value "$version"
+}
+
+################################################################################
+# Get positional arguments                                                     #
+################################################################################
+echo "# Arguments: $@"
+if (( $# < 3 )); then
+  Help
+  exit 0
+else
+  version="$1"; shift
+  application="$1"; shift
+  nameprefix="$1"; shift
+fi
+
+################################################################################
+# Get options                                                                  #
+################################################################################
+while getopts ":h" opt; do
+  case ${opt} in
+    h ) # process option help
+      Help
+      exit 0
+      ;;
+    \? )
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+    : )
+      echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+################################################################################
+# Main Logic                                                                   #
+################################################################################
+printf "# Version\t:$version\n"
+printf "# Application\t:$application\n"
+printf "# Name Prefix\t:$nameprefix\n"
+
+SetVersion $version $application $nameprefix
