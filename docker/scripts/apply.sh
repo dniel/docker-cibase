99f8c71220c10b94499c4a2ab1f356032ee404de
dniel <daniel@engfeldt.net>
2021-06-13 23:01:16 +0200

    set successful exit code

--- docker/scripts/apply.sh
+++ docker/scripts/apply.sh
@@ -132,3 +132,6 @@ printf "# Download Directory\t:$download_dir\n"
 DownloadAndUnzip $s3_url $download_dir
 Kubeconf $kubeconfig_dir $kubeconfig_secret_id
 Apply "$download_dir/$workdir"
+
+# set successful exit code
+exit 0
